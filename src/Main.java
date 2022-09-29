8b2435a4de75206686df9907ebcf74f350b49e74
LocNguyen <nguye2lc@mail.uc.edu>
2022-09-29 12:48:38 -0400

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        double C = 0;
+
+        System.out.print("Please enter C temperature: ");
+        if(in.hasNextDouble())
+        {
+            C = in.nextDouble();
+            in.nextLine();
+
+            double F = C*1.8+32;
+
+            System.out.println("F temperate is: " + F);
+        }
+        else
+        {
+            System.out.println("Please enter a correct input");
+        }
     }
 }
